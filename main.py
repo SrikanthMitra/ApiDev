d6433e1a598a4a58a9d80befaa23590ff011035d
SrikanthMitra <curiousrikanth@gmail.com>
2023-04-02 17:34:20 +0530

    Creating Post Request

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
-
+from fastapi import *
+from fastapi.params import Body
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "hello wrold"}
+    return {"message": "hello world !!sksdksdfkdsj! "}
 
+
+@app.get("/post")
+def get_post():
+    return {"Data": "THis is your post"}
+
+@app.post("/createPost")
+def createPost(payload: dict = Body(...)):
+    print(payload)
+    p1 = payload
+    print(p1.get("title"))
+    return {"Message": "Created Post"}
