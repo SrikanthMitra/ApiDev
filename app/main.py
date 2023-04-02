afffac146dc3c86bd94995cd74e2c6c8a92c9b19
SrikanthMitra <curiousrikanth@gmail.com>
2023-04-02 22:56:21 +0530

    DB Connection

--- app/main.py
+++ app/main.py
@@ -1,26 +1,32 @@
 from fastapi import *
 from fastapi.params import Body
-app = FastAPI()
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
-#import psycpog2
-#from psycpog2.extras import RealDictCursor
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from random import randrange
+import time
+
+app = FastAPI()
+
 class Post(BaseModel):
     title: str
     content: str
     #createtime : datetime.datetime
     publish: bool = False  #setting defalut value as False , only save post. FE will give an notify to publish
     rating: Optional[int] = None
-
-my_posts= [{"title":"First Post","content":"First post content","id":1},{"title":"second Post","content":"second post content","id":2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',database = 'fastapi', user= 'postgres', password = 'password', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Dataase connection sucessfull")
+        break
+    except Exception as error:
+        print("connectiing to DB Failed")
+        print("Error ",error)
+        time.sleep(2)
+#my_posts= [{"title":"First Post","content":"First post content","id":1},{"title":"second Post","content":"second post content","id":2}]
 
 @app.get("/")
 async def root():
@@ -28,30 +34,20 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
-#@app.post("/createPost")
-#def createPost(payload: dict = Body(...)):
-    #print(payload)
-    #p1 = payload
-    #print(p1.get("title"))
-    #return {"Message": "Created Post"}
-#title : String
-#content : string
-#timeofcreate: timedate
-#category :
-#published or draft
-
-@app.post("/posts")
-def createPost(new_post : Post):
-    post_dict = new_post.dict()
-    post_dict['id'] = randrange(0,500)
-    my_posts.append(post_dict)
-    #print(new_post)
-    #print(new_post.dict()) # conveting into a dict for easy readablity
-    return {"data": post_dict}
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
+def createPost(post : Post):
+     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title , post.content, post.publish))
+     new_post = cursor.fetchone()
+     conn.commit()
+     # conveting into a dict for easy readablity
+     return {"data": new_post}
 
 @app.get("/posts/{id}")
 async def get_post(id: int):
-    post = find_post(id)
+    #post = find_post(id)
     return {"data": post}
